2e0e770848ef1bf7e37c6adeff1ffee9a8987b46
Korsakova <korsakova-nastena@yandex.ru>
2019-05-07 20:39:59 +0300

    Car

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
-public class Main {
+public class Car {
 
-    public static void main(String[] args) {
+    public static void Car (String[] args) {
+        String mark;
+        int heft;
+        int power;
+
+        public void power () {
+            Object power;
+            for (int i = 1; power >= i; i ++) {
+                System.out.println(i);
+            }
+        }
+    }
+    class Lorry extends Car {
+        int weight;
+    }
+    public void mark() {
+        System.out.println("ГАЗ");
+    }
+    public void weight() {
+        Object weight;
+        for (int i = 1; i < = weight; i++) {
+            System.out.println(i);
+        }
     }
 }
